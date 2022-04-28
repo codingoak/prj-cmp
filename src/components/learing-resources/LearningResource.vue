226674f5217a448a2bfd8e23f3478fb0af63a20d
Daniel Eicher <d.eicher@mailbox.org>
2022-04-28 18:40:33 +0200

    Add a basebutton to app

--- src/components/learing-resources/LearningResource.vue
+++ src/components/learing-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button :handleClick="buttonTest" mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -16,6 +16,11 @@
 <script>
 export default {
   props: ["title", "description", "link"],
+  methods: {
+    buttonTest() {
+      console.log("it works!");
+    },
+  },
 };
 </script>
 
